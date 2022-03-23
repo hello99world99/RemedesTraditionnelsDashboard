cb50824c7e6a56ee605c72364d4e0c168d837d13
Ibrahim Kelly <57602469+hello99world99@users.noreply.github.com>
2022-03-23 17:28:53 +0000

    Added firebase configuration...

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'remedes-ancestraux',
+    appId: '1:73585650502:web:5e454e42e44fb1bb87d347',
+    storageBucket: 'remedes-ancestraux.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDW-Nm2wQznLHTPEbwsaWwco8ANdCuftfg',
+    authDomain: 'remedes-ancestraux.firebaseapp.com',
+    messagingSenderId: '73585650502',
+    measurementId: 'G-HL3QDCQCH8',
+  },
   production: false
 };
 
